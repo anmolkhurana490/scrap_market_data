298a06b57e386aa18a93c3ec7dd2827f631ed3f6
Anmol Khurana <123490433+anmolkhurana490@users.noreply.github.com>
2025-06-18 12:26:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ const CONFIG = {
 const extractCompanyData = async () => {
     const browser = await puppeteer.launch({
         headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
