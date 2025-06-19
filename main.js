9f0efc77b94bb863c5daa90b86797a1c4c590552
Anmol Khurana <123490433+anmolkhurana490@users.noreply.github.com>
2025-06-19 15:19:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -48,12 +48,10 @@ const extractPdf = async (link, page) => {
                 .then(res => res.arrayBuffer())
                 .then(buf => Array.from(new Uint8Array(buf)))
         );
-        console.log("line51 ran");
 
         fs.writeFileSync(CONFIG.paths.tempPdf, Buffer.from(buffer));
-        console.log("line54 ran");
         const data = fs.readFileSync(CONFIG.paths.tempPdf);
-        console.log("line56 ran");
+        console.log(data);
 
         if (data) {
             const pdf = await PdfParse(data);
